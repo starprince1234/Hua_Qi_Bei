24142211cc289bf8facd12226d465d83819baf76
starprince1234 <2320809136@qq.com>
2026-03-20 23:05:43 +0800

    Delete frontend/src directory

--- frontend/src/components/ResultDisplay.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-interface ResultDisplayProps {
-  result: any;
-}
-
-export default function ResultDisplay({ result }: ResultDisplayProps) {
-  return <pre>{JSON.stringify(result, null, 2)}</pre>;
-}
